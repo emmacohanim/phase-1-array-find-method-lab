ead9fdc1c7e26fd9c015fe388dd469192325cdf8
Emma Cohanim <emmacohanim@gmail.com>
2022-07-27 10:49:59 -0700

    commit tree 3151e2a0932d4707a5a82944aecf6ccb4c915fb2

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // code your solution here
+
+// Use find to isolate specific result
+function superbowlWin(record) {
+  // for of?//
+  //   for (let game of record) {
+  //     if (game.result === "W") {
+  //       // no result in array
+  //       // individual objects have result
+  //       return game.year;
+  //       // returns years when broncos won
+  //     }
+  //   }
+  const game = record.find((g) => {
+    return g.result == "W";
+    // need second return to tell find the results of each conditional (game)
+  });
+
+  if (game) {
+    return game.year;
+  }
+  // if GAME is truthy, return year of game
+
+  // returns undefined by default if never hits return value
+}
+
+const arr = [0, 1, 2, 3];
+
+function find(arr, fn) {
+  for (const e of arr) {
+    if (fn(e)) {
+      return e;
+    }
+  }
+}
+// find returns one result rather than array of results
+
+find(record, (g) => g.result == "W");
+find(record, (g) => g.result == "L");
